4a61d9d66a7ba8896477d4195477e632a62b7e48
Maciej Sieradz <macieks53@gmail.com>
2025-03-29 13:07:55 +0100

    Added db support and register command

--- handler_user.go
+++ handler_user.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+)
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
@@ -8,7 +11,12 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	name := cmd.Args[0]
 
-	err := s.cfg.SetUser(name)
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
