7eb3b78d85bbf6b9c578c51dd10480e238d11370
Maciej Sieradz <macieks53@gmail.com>
2025-08-02 11:45:56 +0200

    added sql support for user management

--- handler_user.go
+++ handler_user.go
@@ -1,8 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/MaciejSieradz/gator/internal/database"
+	"github.com/google/uuid"
 )
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+
+	name := cmd.Args[0]
+
+	createUserParams := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	}
+
+	user, err := s.db.CreateUser(context.Background(), createUserParams)
+	if err != nil {
+		return fmt.Errorf("error creating user: %w", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("error setting current user name: %w", err)
+	}
+
+	fmt.Println("User created successfully")
+	printUser(user)
+	return nil
+}
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
@@ -10,11 +44,21 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	userName := cmd.Args[0]
 
-	err := s.cfg.SetUser(userName)
+	_, err := s.db.GetUser(context.Background(), userName)
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+
+	err = s.cfg.SetUser(userName)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 
 	fmt.Println("User switched successfully!")
 	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:      %v\n", user.ID)
+	fmt.Printf(" * Name:    %v\n", user.Name)
 }
