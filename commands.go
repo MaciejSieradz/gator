30ab316d0c3132c2264d63555aaf1a7f567f5507
Maciej Sieradz <macieks53@gmail.com>
2025-08-02 11:11:03 +0200

    added login handler

---
+++ commands.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type command struct {
+	Name string
+	Args []string
+}
+
+type commands struct {
+	registeredCommands map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.registeredCommands[cmd.Name]
+	if !ok {
+		return fmt.Errorf("handler not found")
+	}
+
+	return handler(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.registeredCommands[name] = f
+}
